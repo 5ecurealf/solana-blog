600700690ae90e28e0d7c807ea92016d4317ec42
5ecurealf <alfielloyd12@gmail.com>
2024-01-17 03:17:29 +0000

    feature/Setup-phantom-connection

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,24 @@
-import { useMemo } from "react"
-import { BlogProvider } from "src/context/Blog"
-import { Router } from "src/router"
-import "./App.css"
-
+import { useMemo } from "react";
+import { BlogProvider } from "src/context/Blog";
+import { Router } from "src/router";
+import "./App.css";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 export const App = () => {
-
+  const endpoint =
+    "https://billowing-fabled-sailboat.solana-devnet.quiknode.pro/bebf42c7c404396271a59508b1767d67b1362ef2/";
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
   return (
-    <BlogProvider>
-      <Router />
-    </BlogProvider>
-  )
-}
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <BlogProvider>
+          <Router />
+        </BlogProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
